c2f4c8b609d219265dd02e745143c7dcd528121d
dead-tech <dev.dead.tech@gmail.com>
2022-06-28 22:12:34 +0200

    :recycle: split interpreting into its own function

--- main.c
+++ main.c
@@ -162,6 +162,66 @@ size_t tokenize(char* file_content, char tokens[][TOKENS_CAP]) {
   return tokens_size;
 }
 
+void interpret(InterpreterState *state)
+{
+  for (size_t i = 0; i < state->tokens_size; ++i) {
+    char* word = state->tokens[state->ip];
+
+#if ENABLE_TRACING
+    print_tokens(tokens, tokens_size);
+    print_stack(stack, stack_size);
+#endif
+
+    if (is_number(word)) {
+      state->stack[state->stack_size++] = atoi(word);
+      ++state->ip;
+    } else if (strcmp("+", word) == 0) {
+      int64_t a = state->stack[--state->stack_size];
+      int64_t b = state->stack[--state->stack_size];
+      state->stack[state->stack_size++] = a + b;
+      ++state->ip;
+    } else if (strcmp("puts", word) == 0) {
+      int64_t index = state->stack[--state->stack_size];
+      printf("%s", state->string_literals[index]);
+      --state->string_literals_size;
+      ++state->ip;
+    } else if (strcmp("putsln", word) == 0) {
+      int64_t index = state->stack[--state->stack_size];
+      printf("%s\n", state->string_literals[index]);
+      --state->string_literals_size;
+      ++state->ip;
+    } else if (strcmp("print", word) == 0) {
+      int64_t top = state->stack[--state->stack_size];
+      printf("%zu", top);
+      ++state->ip;
+    } else if (strcmp("println", word) == 0) {
+      int64_t top = state->stack[--state->stack_size];
+      printf("%zu\n", top);
+      ++state->ip;
+    } else if (strcmp("swap", word) == 0) {
+      int64_t a = state->stack[--state->stack_size];
+      int64_t b = state->stack[--state->stack_size];
+      state->stack[state->stack_size++] = a;
+      state->stack[state->stack_size++] = b;
+      ++state->ip;
+    } else if (strcmp("drop", word) == 0) {
+      __attribute__((unused)) int64_t a = state->stack[--state->stack_size];
+      ++state->ip;
+    } else if (strncmp(word, "\"", 1) == 0) {
+      size_t word_size = strlen(word);
+
+      char quoted_word[word_size - 2];
+      slice_str(word, quoted_word, 1, word_size - 2);
+
+      strcpy(state->string_literals[state->string_literals_size++], quoted_word);
+
+      state->stack[state->stack_size++] = word_size - 2;
+      state->stack[state->stack_size++] = state->string_literals_size - 1;
+      ++state->ip;
+    }
+  }
+}
+
 int main(int argc, char **argv)
 {
   if (argc < 2) {
@@ -174,63 +234,7 @@ int main(int argc, char **argv)
 
   read_file(argv[1], state.file_content);
   state.tokens_size = tokenize(state.file_content, state.tokens);
-
-  for (size_t i = 0; i < state.tokens_size; ++i) {
-    char* word = state.tokens[state.ip];
-
-#if ENABLE_TRACING
-    print_tokens(tokens, tokens_size);
-    print_stack(stack, stack_size);
-#endif
-
-    if (is_number(word)) {
-      state.stack[state.stack_size++] = atoi(word);
-      ++state.ip;
-    } else if (strcmp("+", word) == 0) {
-      int64_t a = state.stack[--state.stack_size];
-      int64_t b = state.stack[--state.stack_size];
-      state.stack[state.stack_size++] = a + b;
-      ++state.ip;
-    } else if (strcmp("puts", word) == 0) {
-      int64_t index = state.stack[--state.stack_size];
-      printf("%s", state.string_literals[index]);
-      --state.string_literals_size;
-      ++state.ip;
-    } else if (strcmp("putsln", word) == 0) {
-      int64_t index = state.stack[--state.stack_size];
-      printf("%s\n", state.string_literals[index]);
-      --state.string_literals_size;
-      ++state.ip;
-    } else if (strcmp("print", word) == 0) {
-      int64_t top = state.stack[--state.stack_size];
-      printf("%zu", top);
-      ++state.ip;
-    } else if (strcmp("println", word) == 0) {
-      int64_t top = state.stack[--state.stack_size];
-      printf("%zu\n", top);
-      ++state.ip;
-    } else if (strcmp("swap", word) == 0) {
-      int64_t a = state.stack[--state.stack_size];
-      int64_t b = state.stack[--state.stack_size];
-      state.stack[state.stack_size++] = a;
-      state.stack[state.stack_size++] = b;
-      ++state.ip;
-    } else if (strcmp("drop", word) == 0) {
-      __attribute__((unused)) int64_t a = state.stack[--state.stack_size];
-      ++state.ip;
-    } else if (strncmp(word, "\"", 1) == 0) {
-      size_t word_size = strlen(word);
-
-      char quoted_word[word_size - 2];
-      slice_str(word, quoted_word, 1, word_size - 2);
-
-      strcpy(state.string_literals[state.string_literals_size++], quoted_word);
-
-      state.stack[state.stack_size++] = word_size - 2;
-      state.stack[state.stack_size++] = state.string_literals_size - 1;
-      ++state.ip;
-    }
-  }
+  interpret(&state);
 
   if (state.stack_size > 0) {
     fprintf(stderr, "petals error: unhandled data on the stack\n");
